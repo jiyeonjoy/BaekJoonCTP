34d18d02435c2ff898fcef06ed796f41dd047175
jiyeonchoi <choi02647@naver.com>
2022-12-28 23:42:56 +0900

    알파벳 찾기

--- BaekJoonCTP.playground/Pages/Step7.xcplaygroundpage/Contents.swift
+++ BaekJoonCTP.playground/Pages/Step7.xcplaygroundpage/Contents.swift
@@ -1,5 +1,21 @@
 import Foundation
 
+func solution2() {
+    let s = "baekjoon" // readLine()!
+    var str:String = ""
+    for a in "abcdefghijklmnopqrstuvwxyz" {
+        let index:Int = Array(s).firstIndex(of: a) ?? -1
+        if str == "" {
+            str += "\(index)"
+        } else {
+            str += " \(index)"
+        }
+    }
+    print(str)
+}
+
+solution2()
+
 func solution1() {
     readLine()
     let line = "54321" //readLine()!
