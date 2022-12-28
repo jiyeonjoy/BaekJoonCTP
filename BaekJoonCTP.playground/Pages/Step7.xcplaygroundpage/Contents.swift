ac11fca68cb3e4aa2e89bcee4d7b315400979368
jiyeonchoi <choi02647@naver.com>
2022-12-29 00:47:15 +0900

    크로아티아 알파벳

--- BaekJoonCTP.playground/Pages/Step7.xcplaygroundpage/Contents.swift
+++ BaekJoonCTP.playground/Pages/Step7.xcplaygroundpage/Contents.swift
@@ -1,5 +1,19 @@
 import Foundation
 
+// 크로아티아 알파벳
+func solution8() {
+    let list:[String] = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
+    var line = "c=c="//readLine()!
+    for s in list {
+        if line.contains(s) {
+            line = line.replacingOccurrences(of: s, with: "a")
+        }
+    }
+    print(line.count)
+}
+
+solution8()
+
 // 다이얼
 func solution7() {
     let dic:[Character:Int] = ["A":3,"B":3,"C":3,"D":4,"E":4,"F":4,"G":5,"H":5,"I":5,"J":6,"K":6,"L":6,"M":7,"N":7,"O":7,"P":8,"Q":8,"R":8,"S":8,"T":9,"U":9,"V":9,"W":10,"X":10,"Y":10,"Z":10]
