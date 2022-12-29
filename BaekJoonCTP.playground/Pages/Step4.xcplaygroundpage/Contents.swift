a64796f9df41309a653b0132167e56acec5e6d93
jiyeonchoi <choi02647@naver.com>
2022-12-30 04:22:48 +0900

    solution3

--- BaekJoonCTP.playground/Pages/Step4.xcplaygroundpage/Contents.swift
+++ BaekJoonCTP.playground/Pages/Step4.xcplaygroundpage/Contents.swift
@@ -15,10 +15,41 @@ func solution7() {}
 func solution6() {}
 func solution5() {}
 func solution4() {}
-func solution3() {}
-func solution2() {}
 
 import Foundation
+
+func solution3() {
+    while true {
+        let line = "2 2"//readLine()!
+        if line == "0 0" {
+            break
+        }
+        let sum = line.split(separator: " ").map{ Int($0)! }.reduce(0,+)
+        print(sum)
+        break
+    }
+
+    while true {
+        let line = "How are you today?" //readLine()!
+        if line == "#" {
+            break
+        }
+        let list:[Character] = ["a", "e", "i", "o", "u","A", "E", "I", "O", "U"]
+        print(line.filter{ list.contains($0) }.count)
+        break
+    }
+}
+
+func solution2() {
+    while true {
+        let line = "!edoc doog a tahW"//readLine()!
+        if line == "END" {
+            break
+        }
+        print(String(line.reversed()))
+        break
+    }
+}
 
 func solution1() {
     let s1 = readLine()!.split(separator: " ").map{ Int($0)! }.reduce(0, +)
