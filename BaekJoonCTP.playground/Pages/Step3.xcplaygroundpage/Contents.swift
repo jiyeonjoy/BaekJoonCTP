ea438f5a5cea5aee27854c5036c6e4ed30cd4442
jiyeonchoi <choi02647@naver.com>
2022-12-30 00:26:06 +0900

    solution1

---
+++ BaekJoonCTP.playground/Pages/Step3.xcplaygroundpage/Contents.swift
@@ -0,0 +1,28 @@
+func solution20() {}
+func solution19() {}
+func solution18() {}
+func solution17() {}
+func solution16() {}
+func solution15() {}
+func solution14() {}
+func solution13() {}
+func solution12() {}
+func solution11() {}
+func solution10() {}
+func solution9() {}
+func solution8() {}
+func solution7() {}
+func solution6() {}
+func solution5() {}
+func solution4() {}
+func solution3() {}
+func solution2() {}
+
+import Foundation
+
+func solution1() {
+    let price:Double = 10000000//Int(readLine()!)!
+    let first = price*0.78
+    let second = price - price*0.2*0.22
+    print("\(Int(first)) \(Int(second))")
+}
