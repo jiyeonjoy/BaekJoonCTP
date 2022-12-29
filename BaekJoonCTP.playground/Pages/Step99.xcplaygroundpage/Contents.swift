11d5a6ab6abd0875a9e9e0713183a5a60085ee7f
jiyeonchoi <choi02647@naver.com>
2022-12-30 04:03:07 +0900

    solution1

---
+++ BaekJoonCTP.playground/Pages/Step99.xcplaygroundpage/Contents.swift
@@ -0,0 +1,22 @@
+func solution20() {}
+func solution19() {}
+func solution18() {}
+func solution17() {}
+func solution16() {}
+func solution15() {}
+func solution14() {}
+func solution13() {}
+func solution12() {}
+func solution11() {}
+func solution10() {}
+func solution9() {}
+func solution8() {}
+func solution7() {}
+func solution6() {}
+func solution5() {}
+func solution4() {}
+func solution3() {}
+func solution2() {}
+func solution1() {}
+
+import Foundation
