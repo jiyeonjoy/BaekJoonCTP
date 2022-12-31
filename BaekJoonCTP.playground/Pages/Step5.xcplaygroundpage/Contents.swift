4e3f996b38eaf2d97f9bc1c921812614b871f439
jiyeonchoi <choi02647@naver.com>
2022-12-31 13:55:38 +0900

    solution34

--- BaekJoonCTP.playground/Pages/Step5.xcplaygroundpage/Contents.swift
+++ BaekJoonCTP.playground/Pages/Step5.xcplaygroundpage/Contents.swift
@@ -17,10 +17,44 @@ func solution38() {}
 func solution37() {}
 func solution36() {}
 func solution35() {}
-func solution34() {}
-func solution33() {}
 
 import Foundation
+
+func solution34() {
+    let dic:[Int:String] = [1995: "ITMO",
+                            1996: "SPbSU",
+                            1997: "SPbSU",
+                            1998: "ITMO",
+                            1999: "ITMO",
+                            2000: "SPbSU",
+                            2001: "ITMO",
+                            2002: "ITMO",
+                            2003: "ITMO",
+                            2004: "ITMO",
+                            2005: "ITMO",
+                            2006: "PetrSU, ITMO",
+                            2007: "SPbSU",
+                            2008: "SPbSU",
+                            2009: "ITMO",
+                            2010: "ITMO",
+                            2011: "ITMO",
+                            2012: "ITMO",
+                            2013: "SPbSU",
+                            2014: "ITMO",
+                            2015: "ITMO",
+                            2016: "ITMO",
+                            2017: "ITMO",
+                            2018: "SPbSU",
+                            2019: "ITMO"]
+    let year = 2003//Int(readLine()!)!
+    let str = dic[year]!
+    print(str)
+}
+
+func solution33() {
+    let num:Double = 1234//Double(readLine()!)!
+    print(sqrt(num)*4)
+}
 
 func solution32() {
     while true {
