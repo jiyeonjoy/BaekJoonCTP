4879bb4a7abd3d19b6fa28408b8e3167639f612b
jiyeonchoi <choi02647@naver.com>
2023-01-04 19:54:41 +0900

    fileIo2

--- BaekJoonCTP.playground/Pages/FileIO.xcplaygroundpage/Contents.swift
+++ BaekJoonCTP.playground/Pages/FileIO.xcplaygroundpage/Contents.swift
@@ -49,3 +49,78 @@ final class FileIO {
 }
 
 let file = FileIO()
+file.readString()
+
+
+//원래는 Class 처리가 없으셨지만 내가 임시로 구현했다.
+class FileIO2 {
+    @inline(__always) private var buffer: [UInt8] = Array(FileHandle.standardInput.readDataToEndOfFile()) + [0], byteIdx = 0
+    
+    @inline(__always) private func readByte() -> UInt8 {
+        defer { byteIdx += 1 }
+        return buffer.withUnsafeBufferPointer { $0[byteIdx] }
+    }
+    
+    @inline(__always) func readInt() -> Int {
+        var number = 0, byte = readByte(), isNegative = false
+        while byte == 10 || byte == 32 { byte = readByte() }
+        if byte == 45 { byte = readByte(); isNegative = true }
+        while 48...57 ~= byte { number = number * 10 + Int(byte - 48); byte = readByte() }
+        return number * (isNegative ? -1 : 1)
+    }
+    
+    @inline(__always) func readStirngSum() -> Int {
+        var byte = readByte()
+        while byte == 10 || byte == 32 { byte = readByte() }
+        var sum = Int(byte)
+        while byte != 10 && byte != 32 && byte != 0 { byte = readByte(); sum += Int(byte) }
+        return sum - Int(byte)
+    }
+    
+    @inline(__always) private func write(_ output: String) {
+        FileHandle.standardOutput.write(output.data(using: .utf8)!)
+    }
+}
+
+struct DoubleStackQueue<Element> {
+    private var inbox: [Element] = []
+    private var outbox: [Element] = []
+    
+    var isEmpty: Bool{
+        return inbox.isEmpty && outbox.isEmpty
+    }
+    
+    var count: Int{
+        return inbox.count + outbox.count
+    }
+    
+    var front: Element? {
+        return outbox.last ?? inbox.first
+    }
+    
+    init() { }
+    
+    init(_ array: [Element]) {
+        self.init()
+        self.inbox = array
+    }
+    
+    mutating func enqueue(_ n: Element) {
+        inbox.append(n)
+    }
+    
+    mutating func dequeue() -> Element {
+        if outbox.isEmpty {
+            outbox = inbox.reversed()
+            inbox.removeAll()
+        }
+        return outbox.removeLast()
+    }
+}
+
+extension DoubleStackQueue: ExpressibleByArrayLiteral {
+    init(arrayLiteral elements: Element...) {
+        self.init()
+        inbox = elements
+    }
+}
