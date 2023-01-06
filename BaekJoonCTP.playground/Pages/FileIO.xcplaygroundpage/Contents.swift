2e020c03dd16d54c1b651e32abe05680f15d9379
jiyeonchoi <choi02647@naver.com>
2023-01-06 17:40:51 +0900

    10989

--- BaekJoonCTP.playground/Pages/FileIO.xcplaygroundpage/Contents.swift
+++ BaekJoonCTP.playground/Pages/FileIO.xcplaygroundpage/Contents.swift
@@ -124,3 +124,75 @@ extension DoubleStackQueue: ExpressibleByArrayLiteral {
         inbox = elements
     }
 }
+
+///
+final class FastIO {
+   private let buffer: [UInt8]
+   private var index: Int = 0
+   private let fhOutput: FileHandle = FileHandle.standardOutput
+
+   init(fhInput: FileHandle = FileHandle.standardInput) {
+       buffer = Array(try! fhInput.readToEnd()!) + [UInt8(0)]
+   }
+
+   @inline(__always) private func read() -> UInt8 {
+       defer {
+           index += 1
+       }
+       return buffer[index]
+   }
+
+   @inline(__always) func readInt() -> Int {
+       var sum = 0
+       var now = read()
+       var isPositive = true
+       while now == 10 || now == 32 {
+           now = read()
+       }
+       if now == 45 {
+           isPositive.toggle(); now = read()
+       }
+       while now >= 48, now <= 57 {
+           sum = sum * 10 + Int(now - 48)
+           now = read()
+       }
+       return sum * (isPositive ? 1 : -1)
+   }
+
+    @inline(__always) func readString() -> String {
+        var now = read()
+        while now == 10 || now == 32 {
+            now = read()
+        }
+        let beginIndex = index-1
+        while now != 10, now != 32, now != 0 {
+            now = read()
+        }
+        return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+    }
+
+   @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+       var now = read()
+       while now == 10 || now == 32 {
+           now = read()
+       }
+       let beginIndex = index - 1
+       while now != 10,
+             now != 32,
+             now != 0 {
+           now = read()
+       }
+       return Array(buffer[beginIndex..<(index - 1)])
+   }
+
+   @inline(__always) func print(_ s: String) {
+       fhOutput.write(s.data(using: .utf8)!)
+   }
+}
+
+let fastIO = FastIO()
+var output = ""
+(1...fastIO.readInt()).forEach { _ in
+    output += "\(fastIO.readInt() + fastIO.readInt())\n"
+}
+fastIO.print(output)
--- BaekJoonCTP.playground/Pages/Step9.xcplaygroundpage/Contents.swift
+++ BaekJoonCTP.playground/Pages/Step9.xcplaygroundpage/Contents.swift
@@ -41,12 +41,195 @@ func solution11() {}
 func solution10() {}
 func solution9() {}
 func solution8() {}
-func solution7() {}
-func solution6() {
-    
-}
 
 import Foundation
+
+// 10989
+func solution7() {
+    final class FastIO {
+       private let buffer: [UInt8]
+       private var index: Int = 0
+       private let fhOutput: FileHandle = FileHandle.standardOutput
+
+       init(fhInput: FileHandle = FileHandle.standardInput) {
+           buffer = Array(try! fhInput.readToEnd()!) + [UInt8(0)]
+       }
+
+       @inline(__always) private func read() -> UInt8 {
+           defer {
+               index += 1
+           }
+           return buffer[index]
+       }
+
+       @inline(__always) func readInt() -> Int {
+           var sum = 0
+           var now = read()
+           var isPositive = true
+           while now == 10 || now == 32 {
+               now = read()
+           }
+           if now == 45 {
+               isPositive.toggle(); now = read()
+           }
+           while now >= 48, now <= 57 {
+               sum = sum * 10 + Int(now - 48)
+               now = read()
+           }
+           return sum * (isPositive ? 1 : -1)
+       }
+
+        @inline(__always) func readString() -> String {
+            var now = read()
+            while now == 10 || now == 32 {
+                now = read()
+            }
+            let beginIndex = index-1
+            while now != 10, now != 32, now != 0 {
+                now = read()
+            }
+            return String(bytes: Array(buffer[beginIndex..<(index-1)]), encoding: .ascii)!
+        }
+
+       @inline(__always) func readByteSequenceWithoutSpaceAndLineFeed() -> [UInt8] {
+           var now = read()
+           while now == 10 || now == 32 {
+               now = read()
+           }
+           let beginIndex = index - 1
+           while now != 10,
+                 now != 32,
+                 now != 0 {
+               now = read()
+           }
+           return Array(buffer[beginIndex..<(index - 1)])
+       }
+
+       @inline(__always) func print(_ s: String) {
+           fhOutput.write(s.data(using: .utf8)!)
+       }
+    }
+
+    struct MinHeap<T: Comparable> {
+        var heap: [T] = []
+        
+        var isEmpty: Bool {
+            return heap.count <= 1 ? true : false
+        }
+        
+        init() {}
+        init(_ element: T) {
+            heap.append(element) // 0번 index채우기 용
+            heap.append(element) // 실제 Root Node 채움.
+        }
+        
+        mutating func insert(_ element: T) {
+            if heap.isEmpty {
+                heap.append(element)
+                heap.append(element)
+                return
+            }
+            heap.append(element)
+            
+            func isMoveUp(_ insertIndex: Int) -> Bool {
+                if insertIndex <= 1 { // Root Node일 때,
+                    return false
+                }
+                let parentIndex = insertIndex / 2
+                return heap[insertIndex] < heap[parentIndex] ? true : false
+            }
+            
+            var insertIndex = heap.count - 1
+            while isMoveUp(insertIndex) {
+                let parentIndex = insertIndex / 2
+                heap.swapAt(insertIndex, parentIndex)
+                insertIndex = parentIndex
+            }
+        }
+        
+        enum moveDownStatus { case left, right, none }
+        
+        mutating func pop() -> T? {
+            if heap.count <= 1 {
+                return nil
+            }
+            let returnData = heap[1]
+            heap.swapAt(1, heap.count - 1)
+            heap.removeLast()
+            
+            func moveDown(_ poppedIndex: Int) -> moveDownStatus {
+                let leftChildIndex = poppedIndex * 2
+                let rightChildIndex = leftChildIndex + 1
+                
+                // case1. 모든(왼쪽) 자식 노드가 없는 경우(완전이진트리는 왼쪽부터 채워지므로)
+                if leftChildIndex >= heap.count {
+                    return .none
+                }
+                
+                // case2. 왼쪽 자식 노드만 있는 경우
+                if rightChildIndex >= heap.count {
+                    return heap[leftChildIndex] < heap[poppedIndex] ? .left : .none
+                }
+                
+                // case3. 왼쪽&오른쪽 자식 노드 모두 있는 경우
+                // case3-1. 자식들이 자신보다 모두 큰 경우(자신이 제일 작은 경우)
+                if (heap[leftChildIndex] > heap[poppedIndex]) && (heap[rightChildIndex] > heap[poppedIndex]) {
+                    return .none
+                }
+                
+                // case3-2. 자식들이 자신보다 모두 작은 경우(왼쪽과 오른쪽 자식 중, 더 작은 자식을 선별)
+                if (heap[leftChildIndex] < heap[poppedIndex]) && (heap[rightChildIndex] < heap[poppedIndex]) {
+                    return heap[leftChildIndex] < heap[rightChildIndex] ? .left : .right
+                }
+                
+                // case3-3. 왼쪽과 오른쪽 자식 중, 한 자식만 자신보다 작은 경우
+                if (heap[leftChildIndex] < heap[poppedIndex]) || (heap[rightChildIndex] < heap[poppedIndex]) {
+                    return heap[leftChildIndex] < heap[rightChildIndex] ? .left : .right
+                }
+                
+                return .none
+            }
+            
+            var poppedIndex = 1
+            while true {
+                switch moveDown(poppedIndex) {
+                case .none:
+                    return returnData
+                case .left:
+                    let leftChildIndex = poppedIndex * 2
+                    heap.swapAt(poppedIndex, leftChildIndex)
+                    poppedIndex = leftChildIndex
+                case .right:
+                    let rightChildIndex = (poppedIndex * 2) + 1
+                    heap.swapAt(poppedIndex, rightChildIndex)
+                    poppedIndex = rightChildIndex
+                }
+            }
+        }
+    }
+
+    let fastIO = FastIO()
+    let n = fastIO.readInt()
+    var arr = [Int](repeating: 0, count: 10001)
+    for _ in 0...n {
+        let n = fastIO.readInt()
+        arr[n] += 1
+    }
+    var answer = ""
+    for i in 1...10000 {
+        answer += String(repeating:"\(i)\n",count:arr[i])
+    }
+    print(answer)
+}
+
+func solution6() {
+    while true {
+        guard let line = readLine() else { break }
+        let list = line.split(separator: " ").map{ Int($0)! }
+        let v = max(list[1]-list[0], list[2]-list[1])-1
+        print(v)
+    }
+}
 
 // 5692
 func solution5() {
