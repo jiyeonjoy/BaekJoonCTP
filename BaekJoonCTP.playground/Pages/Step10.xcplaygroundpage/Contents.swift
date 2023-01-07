ef5b4e93837d9a16d47c0ce065c3ef0a1916c950
jiyeonchoi <choi02647@naver.com>
2023-01-07 17:25:07 +0900

    added step10

---
+++ BaekJoonCTP.playground/Pages/Step10.xcplaygroundpage/Contents.swift
@@ -0,0 +1,55 @@
+let list = readLine()!.split(separator: " ").map{ Int($0)! }
+Int(readLine()!)!
+
+func solution50() {}
+func solution49() {}
+func solution48() {}
+func solution47() {}
+func solution46() {}
+func solution45() {}
+func solution44() {}
+func solution43() {}
+func solution42() {}
+func solution41() {}
+func solution40() {}
+func solution39() {}
+func solution38() {}
+func solution37() {}
+func solution36() {}
+func solution35() {}
+func solution34() {}
+func solution33() {}
+func solution32() {}
+func solution31() {}
+func solution30() {}
+func solution29() {}
+func solution28() {}
+func solution27() {}
+func solution26() {}
+func solution25() {}
+func solution24() {}
+func solution23() {}
+func solution22() {}
+func solution21() {}
+func solution20() {}
+func solution19() {}
+func solution18() {}
+func solution17() {}
+func solution16() {}
+func solution15() {}
+func solution14() {}
+func solution13() {}
+func solution12() {}
+func solution11() {}
+func solution10() {}
+func solution9() {}
+func solution8() {}
+func solution7() {}
+func solution6() {}
+func solution5() {}
+func solution4() {}
+func solution3() {}
+func solution2() {}
+func solution1() {}
+
+import Foundation
