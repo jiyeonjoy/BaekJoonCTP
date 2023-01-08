b270e38e46eb3b956c9a51c2d2ccd13d2e71ce72
jiyeonchoi <choi02647@naver.com>
2023-01-08 22:47:21 +0900

    1018

--- BaekJoonCTP.playground/Pages/Step0.xcplaygroundpage/Contents.swift
+++ BaekJoonCTP.playground/Pages/Step0.xcplaygroundpage/Contents.swift
@@ -1,6 +1,26 @@
 let list = readLine()!.split(separator: " ").map{ Int(String($0))! }
 Int(readLine()!)!
 
+let k:Double = 5//Double(readLine()!)!
+var list:[Int] = [1,3,8,2,-2]
+var dic:[Int:Int] = [:]
+for n in list {
+    let c = dic[n] ?? 0
+    dic[n] = c+1
+}
+
+//for _ in 1...Int(k) {
+//    let n = Int(readLine()!)!
+//    list.append(n)
+//    let c = dic[n] ?? 0
+//    dic[n] = c+1
+//}
+list.sort()
+print(Int(round(Double(list.reduce(0,+))/k)))
+print(list[Int(k)/2])
+
+print(list.max()!-list.min()!)
+
 func solution50() {}
 func solution49() {}
 func solution48() {}
--- BaekJoonCTP.playground/Pages/Step11.xcplaygroundpage/Contents.swift
+++ BaekJoonCTP.playground/Pages/Step11.xcplaygroundpage/Contents.swift
@@ -13,7 +13,48 @@ func solution41() {}
 func solution40() {}
 func solution39() {}
 func solution38() {}
-func solution37() {}
+
+// 1018
+func solution37() {
+    let list = readLine()!.split(separator: " ").map{ Int(String($0))! }
+    let n = list[0]
+    let m = list[1]
+    var pan:[[Character]] = []
+    for _ in 1...n {
+        pan.append(Array(readLine()!))
+    }
+    var minv = 64
+    for i in 0...n-8 {
+        for j in 0...m-8 {
+            var mina = 0
+            var minb = 0
+            for k in 0...7 {
+                for l in 0...7 {
+                    if (i+j+k+l)%2 == 0 {
+                        if pan[i+k][j+l] == "B" {
+                            mina += 1
+                        } else {
+                            minb += 1
+                        }
+                    } else {
+                        if pan[i+k][j+l] == "W" {
+                            mina += 1
+                        } else {
+                            minb += 1
+                        }
+                    }
+                }
+            }
+            if minv > mina {
+                minv = mina
+            }
+            if minv > minb {
+                minv = minb
+            }
+        }
+    }
+    print(minv)
+}
 
 // 25305
 func solution36() {
