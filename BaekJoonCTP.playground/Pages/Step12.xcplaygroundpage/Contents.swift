cc5236913b805c43619064e5555ba6f6b09e9908
jiyeonchoi <choi02647@naver.com>
2023-01-10 01:12:16 +0900

    1654

--- BaekJoonCTP.playground/Pages/Step12.xcplaygroundpage/Contents.swift
+++ BaekJoonCTP.playground/Pages/Step12.xcplaygroundpage/Contents.swift
@@ -1,5 +1,3 @@
-import Foundation
-
 // 1436
 //let n = 10000//Int(readLine()!)!
 //var v = 666
@@ -22,3 +20,35 @@ import Foundation
 //    }
 //}
 //print(v)
+import Foundation
+
+// 1654
+func s1654() {
+    let list = readLine()!.split(separator: " ").map{ Int(String($0))! }
+    let k = list[1]
+    var l:[Int] = []
+    var minv = 0
+    var maxv = 2147483648
+    for _ in 1...list[0] {
+        l.append(Int(readLine()!)!)
+    }
+    while maxv-minv > 1 {
+        let mv = (maxv+minv)/2
+        var sum = 0
+        for i in l {
+             if i/mv > k {
+                    break
+                }
+                sum += i/mv
+                if sum > k {
+                    break
+                }
+        }
+        if sum < k {
+            maxv = mv
+        } else {
+            minv = mv
+        }
+    }
+    print(minv)
+}
