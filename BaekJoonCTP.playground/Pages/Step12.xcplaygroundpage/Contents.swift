5cdf175de140a09c03e87ad8ebf47dffbae2a3c1
jiyeonchoi <choi02647@naver.com>
2023-01-11 04:25:16 +0900

    s1436

--- BaekJoonCTP.playground/Pages/Step12.xcplaygroundpage/Contents.swift
+++ BaekJoonCTP.playground/Pages/Step12.xcplaygroundpage/Contents.swift
@@ -1,26 +1,22 @@
-// 1436
-//let n = 10000//Int(readLine()!)!
-//var v = 666
-//var c = 1
-//var d:[Int:Int] = [
-//    667:1665,
-//    9666:10665,
-//    99666:100665,
-//    999666:1000665,
-//]
-//while n != c {
-//    v += 1
-//    var vv = v
-//    while vv >= 666 {
-//        if vv%1000==666 {
-//            c += 1
-//            break
-//        }
-//        vv /= 10
-//    }
-//}
-//print(v)
 import Foundation
+
+func s1436() {
+    let n = Int(readLine()!)!
+    var v = 666
+    var c = 1
+    while n != c {
+        v += 1
+        var i = v
+        while i >= 666 {
+            if i%1000 == 666 {
+                c += 1
+                break
+            }
+            i /= 10
+        }
+    }
+    print(v)
+}
 
 func getPow(_ i:Int) -> Int {
     if i == 0 {
