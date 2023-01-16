f86b7294b1774d7cc7dde51ae0fe67a9500eb468
jiyeonchoi <choi02647@naver.com>
2023-01-16 21:15:18 +0900

    added fileio2

---
+++ BaekJoonCTP.playground/Pages/FileIO2.xcplaygroundpage/Contents.swift
@@ -0,0 +1,69 @@
+import Foundation
+
+final class FileIO {
+    private let buffer: Data
+    private var index: Int = 0
+
+    init(fileHandle: FileHandle = FileHandle.standardInput) {
+        self.buffer = try! fileHandle.readToEnd()! // 인덱스 범위 넘어가는 것 방지
+    }
+
+    @inline(__always) private func read() -> UInt8 {
+        defer {
+            index += 1
+        }
+        guard index < buffer.count else { return 0 }
+
+        return buffer[index]
+    }
+
+    @inline(__always) func readInt() -> Int {
+        var sum = 0
+        var now = read()
+        var isPositive = true
+
+        while now == 10
+                || now == 32 { now = read() } // 공백과 줄바꿈 무시
+        if now == 45 { isPositive.toggle(); now = read() } // 음수 처리
+        while now >= 48, now <= 57 {
+            sum = sum * 10 + Int(now-48)
+            now = read()
+        }
+
+        return sum * (isPositive ? 1:-1)
+    }
+
+
+    @inline(__always) func readString() -> String {
+            var str = ""
+            var now = read()
+
+            while now == 10
+                    || now == 32 { now = read() } // 공백과 줄바꿈 무시
+
+            while now != 10
+                    && now != 32 && now != 0 {
+                str += String(bytes: [now], encoding: .ascii)!
+                now = read()
+            }
+
+            return str
+        }
+  }
+
+let file = FileIO()
+let caseNum = file.readInt()
+
+var integerNum = 0
+var integers = 0
+
+for _ in 1...caseNum {
+    integerNum = file.readInt()
+    var integerArr = [Int]()
+    for _ in 1...integerNum {
+        integers = file.readInt()
+        integerArr.append(integers)
+    }
+    
+    print(integerArr.min()!,integerArr.max()!)
+}
