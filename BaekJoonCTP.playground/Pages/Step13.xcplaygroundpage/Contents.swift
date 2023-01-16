d28cd0987e83126d776ed256d7f4540dfa1133e3
jiyeonchoi <choi02647@naver.com>
2023-01-16 21:32:42 +0900

    s2061

--- BaekJoonCTP.playground/Pages/Step13.xcplaygroundpage/Contents.swift
+++ BaekJoonCTP.playground/Pages/Step13.xcplaygroundpage/Contents.swift
@@ -1,5 +1,41 @@
 import Foundation
 
+func s2061() {
+    func isPrime(num: Int) -> Bool {
+        if(num<4) {
+            return num == 1 ? false : true
+        }
+        for i in 2...Int(sqrt(Double(num))) {
+            if(num % i == 0) { return false }
+        }
+        return true
+    }
+
+    func mod(_ s:String, _ p:Int) -> Int {
+        var ret = 0
+        for c in s {
+            ret = (ret*10+Int(String(c))!)%p
+        }
+        return ret
+    }
+
+    let l = readLine()!.split(separator: " ").map{ String($0) }
+    let k = l[0]
+    let m = Int(l[1])!
+    var a = -1
+    for i in 1...m-1 {
+        if isPrime(num: i) && mod(k, i) == 0 {
+            a = i
+            break
+        }
+    }
+    if a == -1 {
+        print("GOOD")
+    } else {
+        print("BAD \(a)")
+    }
+}
+
 func s3036() {
     func gcd(_ a: Int, _ b: Int) -> Int{
         if (b == 0) { return a }
