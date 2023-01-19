a8c6ca04e7381b495d6c6a3ad62b68906699eb37
jiyeonchoi <choi02647@naver.com>
2023-01-19 21:28:49 +0900

    dfs / bfs

--- BaekJoonCTP.playground/Pages/DFS_BFS.xcplaygroundpage/Contents.swift
+++ BaekJoonCTP.playground/Pages/DFS_BFS.xcplaygroundpage/Contents.swift
@@ -1,54 +1,22 @@
 import Foundation
 
-//let l = readLine()!.split(separator: " ").map{ Int(String($0))! }
-let l = "4 5 1".split(separator: " ").map{ Int(String($0))! }
-//let l = "5 5 3".split(separator: " ").map{ Int(String($0))! }
-
-let n = l[0]
-let m = l[1]
-let v = l[2]
-
-var g:[[Int]] = [
-    [1, 2],
-    [1, 3],
-    [1, 4],
-    [2, 4],
-    [3, 4],
+let graph = [
+    [], // 0
+    [2,3], // 1
+    [1,4,5], // 2
+    [1,6,7], // 3
+    [2], // 4
+    [2], // 5
+    [3], // 6
+    [3,8], // 7
+    [7] // 8
 ]
-//var g:[[Int]] = [
-//    [5, 4],
-//    [5, 2],
-//    [1, 2],
-//    [3, 4],
-//    [3, 1],
-//]
 
-//for _ in 1...m {
-//    let l = readLine()!.split(separator: " ").map{ Int(String($0))! }
-//    g.append(l)
-//}
-
-var graph:[[Int]] = Array.init(repeating: [], count: n+1)
-var graph2:[[Int]] = Array.init(repeating: [], count: n+1)
-for l in g {
-    var ll = graph[l[0]]
-    ll.append(l[1])
-    graph[l[0]] = ll.sorted()
-    graph2[l[0]] = ll.sorted()
-
-    var lll = graph2[l[1]]
-    lll.append(l[0])
-    graph[l[1]] = lll.sorted()
-    graph2[l[1]] = lll.sorted()
-}
-
-
-var visited = Array.init(repeating: false, count: graph.count+1)
-var visited2 = Array.init(repeating: false, count: graph.count+1)
+var visited = Array.init(repeating: false, count: graph.count)
 
 func dfs(start: Int) {
   visited[start] = true // 시작점
-
+  
   print(start, terminator: " ")
 
   for i in graph[start] { // 왼쪽부터 순회
@@ -58,23 +26,20 @@ func dfs(start: Int) {
   }
 }
 
-dfs(start: v)
-print("")
-
 var queue = Queue<Int>()
 // bfs
 func bfs(start: Int) {
   queue.enquque(start) // 시작점 큐에 넣기
-  visited2[start] = true // 시작점 방문으로 체크
+  visited[start] = true // 시작점 방문으로 체크
   
   while !queue.isEmpty {
     guard let elem = queue.dequeue() else { return }
     print(elem, terminator: " ")
-    
-    for i in graph2[elem] {
-      if !visited2[i] {
+
+    for i in graph[elem] {
+      if !visited[i] {
         queue.enquque(i)
-          visited2[i] = true
+        visited[i] = true
       }
     }
   }
@@ -82,19 +47,19 @@ func bfs(start: Int) {
 
 public struct Queue<T> {
   fileprivate var array = [T]()
-  
+
   public var isEmpty: Bool {
     return array.isEmpty
   }
-  
+
   public var count: Int {
     return array.count
   }
-  
+
   public mutating func enquque(_ element: T) {
     array.append(element)
   }
-  
+
   public mutating func dequeue() -> T? {
     if isEmpty {
       return nil
@@ -102,10 +67,11 @@ public struct Queue<T> {
       return array.removeFirst()
     }
   }
-  
+
   public var front: T? {
     return array.first
   }
 }
 
-bfs(start: v)
+dfs(start: 1)
+bfs(start: 1)
