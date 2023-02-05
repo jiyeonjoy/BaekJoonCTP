1fa9bed88ee68de3d36d11e06f763b53f8763129
jiyeonchoi <choi02647@naver.com>
2023-02-05 20:29:38 +0900

    s16916

--- BaekJoonCTP.playground/Pages/Step14.xcplaygroundpage/Contents.swift
+++ BaekJoonCTP.playground/Pages/Step14.xcplaygroundpage/Contents.swift
@@ -1,5 +1,64 @@
 import Foundation
 
+func KMP(source:[String], target:[String]) -> Bool {
+    let sourceCnt = source.count
+    let targetCnt = target.count
+    
+    var LPS = Array(repeating: 0, count: targetCnt)
+    LPS = LPScompute(target:target, LPS:LPS)
+    
+    var sourceIdx = 0
+    var targetIdx = 0
+    
+    while (sourceIdx < sourceCnt) {
+        if target[targetIdx] == source[sourceIdx] {
+            sourceIdx += 1
+            targetIdx += 1
+        } else {
+            if targetIdx == 0 {
+                sourceIdx += 1
+            } else {
+                targetIdx = LPS[targetIdx - 1]
+            }
+        }
+        if targetIdx == targetCnt {
+            return true
+        }
+    }
+    return false
+}
+
+func LPScompute(target:[String], LPS:[Int]) -> [Int] {
+    var length = 0
+    var idx = 1
+    var LPS = LPS
+    while (idx < target.count) {
+        if target[idx] == target[length] {
+            length += 1
+            LPS[idx] = length
+            idx += 1
+        } else {
+            if length == 0 {
+                LPS[idx] = 0
+                idx += 1
+            } else {
+                length = LPS[length - 1]
+            }
+        }
+    }
+    return LPS
+}
+
+func s16916() {
+    let s = Array(readLine()!.map{String($0)})
+    let p = Array(readLine()!.map{String($0)})
+    if KMP(source: s, target: p) {
+        print(1)
+    } else {
+        print(0)
+    }
+}
+
 func s2495() {
     while true {
         guard let line = readLine() else { break }
