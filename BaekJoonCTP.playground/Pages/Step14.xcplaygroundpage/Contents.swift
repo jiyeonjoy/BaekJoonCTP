61d31f02da8285b63ce40cfb98ce2e15491f82a6
jiyeonchoi <choi02647@naver.com>
2023-02-05 18:11:46 +0900

    s5218

--- BaekJoonCTP.playground/Pages/Step14.xcplaygroundpage/Contents.swift
+++ BaekJoonCTP.playground/Pages/Step14.xcplaygroundpage/Contents.swift
@@ -1,5 +1,49 @@
 import Foundation
 
+func s5218() {
+    let t = Int(readLine()!)!
+    let alphabet:[Character:Int] = [
+        "A":1,
+        "B":2,
+        "C":3,
+        "D":4,
+        "E":5,
+        "F":6,
+        "G":7,
+        "H":8,
+        "I":9,
+        "J":10,
+        "K":11,
+        "L":12,
+        "M":13,
+        "N":14,
+        "O":15,
+        "P":16,
+        "Q":17,
+        "R":18,
+        "S":19,
+        "T":20,
+        "U":21,
+        "V":22,
+        "W":23,
+        "X":24,
+        "Y":25,
+        "Z":26,
+    ]
+    for _ in 1...t {
+        var list = readLine()!.split(separator: " ").map{ String($0) }
+        let arr1 = Array(list[0])
+        let arr2 = Array(list[1])
+        var str = "Distances:"
+        for i in 0...arr1.count-1 {
+            let s1 = alphabet[arr1[i]] ?? 0
+            let s2 = alphabet[arr2[i]] ?? 0
+            str.write(s1 <= s2 ? " \(s2-s1)" : " \(s2-s1+26)")
+        }
+        print(str)
+    }
+}
+
 func s14697() {
     var list = readLine()!.split(separator: " ").map{ Int(String($0))! }
     let a = list[0]
