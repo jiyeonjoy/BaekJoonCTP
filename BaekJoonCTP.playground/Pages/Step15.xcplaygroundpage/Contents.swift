0fc52f30bf5854060b0c34afe8b4a23f72b88fef
최지연 <choi02647@naver.com>
2023-02-15 16:37:41 +0900

    s16088

---
+++ BaekJoonCTP.playground/Pages/Step15.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+import Foundation
+
+func s16088() {
+    let n = Int(readLine()!)!
+    for _ in 1...n {
+        var list = readLine()!.split(separator: " ").map{ Int(String($0))! }
+        let l = list[0]
+        let r = list[1]
+        let n = list[2]
+        let m = list[3]
+        if n == m {
+            print("G")
+        } else {
+            let a = abs(n-m)
+            if a <= l && a > r {
+                print("L")
+            } else if a <= r && a > l {
+                print("R")
+            } else {
+                print("E")
+            }
+        }
+    }
+}
