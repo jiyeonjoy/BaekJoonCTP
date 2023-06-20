1b9b3ffc147a63d34c446a9a4cbfd759b9235862
최지연 <choi02647@naver.com>
2023-06-20 11:49:03 +0900

    added buffer

---
+++ BaekJoonCTP.playground/Pages/Buffer.xcplaygroundpage/Contents.swift
@@ -0,0 +1,25 @@
+import Foundation
+
+var buffer: [UInt8] = Array(FileHandle.standardInput.readDataToEndOfFile()), byteIdx = 0; buffer.append(0)
+
+@inline(__always) func readByte() -> UInt8 {
+    defer { byteIdx += 1 }
+    return buffer.withUnsafeBufferPointer { $0[byteIdx] }
+}
+
+@inline(__always) func readInt() -> Int {
+    var number = 0, byte = readByte(), isNegative = false
+    while byte == 10 || byte == 32 { byte = readByte() }
+    if byte == 45 { byte = readByte(); isNegative = true }
+    while 48...57 ~= byte { number = number * 10 + Int(byte - 48); byte = readByte() }
+    return number * (isNegative ? -1 : 1)
+}
+
+// 6
+// 1 3 2 5 8 1
+var answer = 0, current = 0
+for _ in 0..<readInt() {
+    let tower = readInt()
+    if current <= tower { answer += 1 }
+    current = tower
+}
